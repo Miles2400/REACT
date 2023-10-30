b599a2cea6ce5c9bba05915b46e0b10dc1e1f638
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-30 23:17:23 +0100

    setups

---
+++ gtp-3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container';
+import { CTA, Brand, Navbar } from './components';
+
+const App = () => {
+    return (
+        <div>
+            <h1>GTP- </h1>
+        </div>
+    )
+}
+
+export default App
