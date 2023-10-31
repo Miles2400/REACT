6e76452d3b21ff1e8f535bc512286f22fc53f514
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-31 15:27:19 +0100

    making the nav bar responsive (incomplete)

--- gpt-3/src/components/navbar/Navbar.jsx
+++ gpt-3/src/components/navbar/Navbar.jsx
@@ -43,6 +43,10 @@ const Navbar = () => {
 					<div className='gpt3__navbar-menu_container scale-up-center'>
 						<div className='gpt3__navbar-menu_container-links'>
 							<Menu />
+							<div className="gpt3__navbar-menu_container-links-sign">
+								<p>Sign in</p>
+								<button type="button">Sign up</button>
+							</div>
 						</div>
 					</div>
 				)}
