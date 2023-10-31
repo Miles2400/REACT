2d727bd238c5366ea1f59f9277382c7047a5f7e6
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-31 15:05:01 +0100

    added toggle menu for mobile devices (still incomplete)

--- gpt-3/src/components/navbar/Navbar.jsx
+++ gpt-3/src/components/navbar/Navbar.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.svg'
-//import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+function Menu() {
+	return (
+		<>
+			<p> <a href="#home">Home</a> </p>
+			<p> <a href="#wgpt3">What is GPT3?</a> </p>
+			<p> <a href="#possibility">Open AI</a> </p>
+			<p> <a href="#blog">Library</a> </p>
+		</>
+	)
+};
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
 	return (
-		<div claasName="gpt3__navbar">
+		<div className="gpt3__navbar">
+
 			<div className="gpt3__navbar-links">
 				<div className="gpt3__navbar-links-logo">
 					<img src={logo} alt="logo" />
 				</div>
 				<div className="gpt3__navbar-links-container">
-					<p> <a href="#home">Home</a> </p>
-					<p> <a href="#wgpt3">What is GPT3?</a> </p>
-					<p> <a href="#possibility">Open AI</a> </p>
-					<p> <a href="#blog">Library</a> </p>
+					<Menu />
 				</div>
 			</div>
+
+			<div className="gpt3__navbar-sign">
+				<p>Sign in</p>
+				<button type="button">Sign up</button>
+			</div>
+
+			<div className="gpt3__navbar-menu">
+				{toggleMenu
+					? <RiCloseLine color="#ffff" size={27} onClick={() => setToggleMenu(false)} />
+					: <RiMenu3Line color="#ffff" size={27} onClick={() => setToggleMenu(true)} />
+				}
+				{toggleMenu && (
+					<div className='gpt3__navbar-menu_container scale-up-center'>
+						<div className='gpt3__navbar-menu_container-links'>
+							<Menu />
+						</div>
+					</div>
+				)}
+			</div>
+
 		</div>
 	)
 }
