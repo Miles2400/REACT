d227c1fad0e36197a00e865b3f9e1a1e35da14a4
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-10-31 13:15:14 +0100

    added the required assets

--- gpt-3/src/App.jsx
+++ gpt-3/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-//import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container';
-//import { CTA, Brand, Navbar } from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello world!</h1>
-        </div>
-    )
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<Navbar />
+				<Header />
+			</div>
+			<div>
+				<Brand />
+				<WhatGPT3 />
+				<Features />
+				<Possibility />
+				<CTA />
+				<Blog />
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 export default App
--- gpt-3/src/components/navbar/Navbar.jsx
+++ gpt-3/src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.svg'
+//import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
+	return (
+		<div claasName="gpt3__navbar">
+			<div className="gpt3__navbar-links">
+				<div className="gpt3__navbar-links-logo">
+					<img src={logo} alt="logo" />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Navbar
