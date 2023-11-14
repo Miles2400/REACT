b6e105aee36a23e40ef154cc663de74fce36e890
Miles2400 <132457577+Miles2400@users.noreply.github.com>
2023-11-14 10:20:13 +0100

    update

--- restaurant/src/components/Navbar/Navbar.jsx
+++ restaurant/src/components/Navbar/Navbar.jsx
@@ -23,13 +23,16 @@ const Navbar = () => (
 		</div>
 		<div className="app__navbar-smallscreen">
 			<GiHamburgerMenu color="#fff" fontSize={27} onClick={() => { }} />
-			<ul className='app__navbar-smallscreen-links'>
-				<li className='p__opensans'> <a href="#home">Home</a> </li>
-				<li className='p__opensans'> <a href="#about">About</a> </li>
-				<li className='p__opensans'> <a href="#menu">Menu</a> </li>
-				<li className='p__opensans'> <a href="#awards">Awards</a> </li>
-				<li className='p__opensans'> <a href="#contact">Contact</a> </li>
-			</ul>
+			<div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+				<MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => { }} />
+				<ul className='app__navbar-smallscreen-links'>
+					<li className='p__opensans'> <a href="#home">Home</a> </li>
+					<li className='p__opensans'> <a href="#about">About</a> </li>
+					<li className='p__opensans'> <a href="#menu">Menu</a> </li>
+					<li className='p__opensans'> <a href="#awards">Awards</a> </li>
+					<li className='p__opensans'> <a href="#contact">Contact</a> </li>
+				</ul>
+			</div>
 		</div>
 	</nav>
 );
