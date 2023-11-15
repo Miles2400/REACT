657c8dfd9fa74a4d168990e5314ad6d04bd832ed
Miles240 <piuschukwuebuka40@gmail.com>
2023-11-15 12:29:11 +0100

    started the header section

--- restaurant/src/components/Navbar/Navbar.jsx
+++ restaurant/src/components/Navbar/Navbar.jsx
@@ -9,9 +9,11 @@ const Navbar = () => {
 
 	return (
 		<nav className='app__navbar'>
+
 			<div className='app__navbar-logo'>
 				<img src={images.gericht} alt='app logo' />
 			</div>
+
 			<ul className='app__navbar-links'>
 				<li className='p__opensans'> <a href="#home">Home</a> </li>
 				<li className='p__opensans'> <a href="#about">About</a> </li>
@@ -19,11 +21,13 @@ const Navbar = () => {
 				<li className='p__opensans'> <a href="#awards">Awards</a> </li>
 				<li className='p__opensans'> <a href="#contact">Contact</a> </li>
 			</ul>
+
 			<div className="app__navbar-login">
 				<a href="#login" className="p__opensans">Log In / Register</a>
 				<div />
 				<a href="/" className="p__opensans">Book Table</a>
 			</div>
+
 			<div className="app__navbar-smallscreen">
 				<GiHamburgerMenu color="#fff" fontSize={27} onClick={() => { setToggleMenu(true) }} />
 
@@ -40,6 +44,7 @@ const Navbar = () => {
 					</div>
 				)}
 			</div>
+
 		</nav>
 	)
 };
--- restaurant/src/container/Header/Header.jsx
+++ restaurant/src/container/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+	<div className='app__header app__wrapper section__padding' id='home'>
+		<div className="app__wrapper_info">
+			<SubHeading title="Chase The New Flavor" />
+			<h1 className="app__header-h1">The Key To Fine Dinning</h1>
+			<p className="p__opensans" style={{ margin: '2rem 0' }}>
+				Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
+			</p>
+			<button className="custom__button" type="button">Explore Menu</button>
+		</div>
+		<div className="app__wrapper_img">
+			<img src={images.welcome} alt="header image" />
+		</div>
+	</div>
 );
 
 export default Header;
