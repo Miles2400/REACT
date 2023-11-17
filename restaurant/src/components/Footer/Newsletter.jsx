1d0ce1c8dca65893b13cae43dffa31bfaf6e860e
Miles2400 <134848387+Miles240@users.noreply.github.com>
2023-11-17 16:17:24 +0100

    started the footer sectiom

--- restaurant/src/components/Footer/Newsletter.jsx
+++ restaurant/src/components/Footer/Newsletter.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
+	<div className='app__newsletter'>
+		<div className="app__newsletter-heading">
+			<SubHeading title={"Newsletter"} />
+			<h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+			<p className="p__opensans">And never miss latest Updates!</p>
+		</div>
+	</div>
 );
 
 export default Newsletter;
