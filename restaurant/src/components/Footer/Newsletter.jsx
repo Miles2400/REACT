fa384fdd3e5ed925d9027bbc1bdc75f1cfb52b15
Miles2400 <piusjay40@gmail.com>
2023-11-18 18:15:31 +0100

    finished the news letter

--- restaurant/src/components/Footer/Newsletter.jsx
+++ restaurant/src/components/Footer/Newsletter.jsx
@@ -7,7 +7,7 @@ const Newsletter = () => (
 		<div className="app__newsletter-heading">
 			<SubHeading title={"Newsletter"} />
 			<h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
-			<p className="p__opensans">And never miss latest Updates!</p>
+			<p className="p__cormorant">And never miss latest Updates!</p>
 		</div>
 		<div className="app__newsletter-input flex__center">
 			<input type='email' placeholder="Enter address" />
