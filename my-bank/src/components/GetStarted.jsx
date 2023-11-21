fb1817068f70b1277d4956c7e69b19a151bb194d
Miles2400 <piusjay40@gmail.com>
2023-11-21 19:27:16 +0100

    completed

--- my-bank/src/components/GetStarted.jsx
+++ my-bank/src/components/GetStarted.jsx
@@ -5,7 +5,7 @@ import { arrowUp } from '../assets'
 const GetStarted = () => (
 	<div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
 
-		<div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+		<div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full hover:bg-slate-900`} >
 			<div className={`${styles.flexStart} flex-row`}>
 				<p className='font-poppins font-medium text-[18px] leading-[23px] mr-1.5'>
 					<span className='text-gradient font-semibold'>Get</span>
