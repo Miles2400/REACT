dee0f56218b1ead82e5e219da7d9b840cc056daf
Miles2400 <piusjay40@gmail.com>
2023-11-21 16:40:43 +0100

    started the hero section

--- my-bank/src/components/Hero.jsx
+++ my-bank/src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
 
-const Hero = () => {
-    return (
-        <div>Hero</div>
-    )
-}
+
+const Hero = () => (
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+            <div className="flex flex-row items-center px-4 bg-discount-gradient py-[6px] rounded-[10px] mb-2">
+                <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
+                <span className="text-white">20%</span>
+            </div>
+        </div>
+    </section>
+)
+
 
 export default Hero
