c4a32d6d330f05ee95c45787a0b9b6ade9748599
Miles2400 <piusjay40@gmail.com>
2023-11-21 19:27:01 +0100

    finish the hero section

--- my-bank/src/components/Hero.jsx
+++ my-bank/src/components/Hero.jsx
@@ -33,8 +33,15 @@ const Hero = () => (
             </p>
         </div>
 
-        <div className=''>
-            <img src={robot} alt="" />
+        <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+            <img src={robot} alt="robot" className='w-[100%] h-[100%] z-[5] relative' />
+            <div className='absolute z-0 w-[40%] h-[35%] top-0 pink__gradient' />
+            <div className='absolute z-1 w-[80%] h-[80%] bottom-40 white__gradient rounded-full' />
+            <div className='absolute z-0 w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+        </div>
+
+        <div className={`ss:hidden ${styles.flexCenter}`}>
+            <GetStarted />
         </div>
     </section>
 )
