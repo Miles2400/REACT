b4ccc06b21849a853fdf1257f5d307c6e2f84674
Miles2400 <piusjay40@gmail.com>
2023-11-22 10:54:15 +0100

    created a reuseable button conponent

--- my-bank/src/components/Button.jsx
+++ my-bank/src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+// import 
 
-const Button = () => {
+const Button = ({ styles }) => {
     return (
-        <div>Button</div>
+        <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[8px] ${styles}`}>
+            Get Started
+        </button>
     )
 }
 
