eea4f787e9823ba42d6c21ba6cab1d5a6f4b75a2
Miles2400 <piusjay40@gmail.com>
2023-11-23 10:29:27 +0100

    ../

--- my-bank/src/components/Navbar.jsx
+++ my-bank/src/components/Navbar.jsx
@@ -29,10 +29,10 @@ const Navbar = () => {
 					onClick={() => setToggle((prev) => !prev)}
 				/>
 
-				<div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient top-20 right-0 absolute mx-4 my-2 min-w[140px] rounded-xl sidebar`}>
+				<div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient top-20 right-0 absolute mx-4 my-2 min-w-[130px] rounded-xl sidebar`}>
 					<ul className="list-none flex justify-end items-center flex-1 flex-col">
 						{navLinks.map((nav, index) => (
-							<li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} `}>
+							<li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} hover:bg-slate-600 w-[100%] text-center rounded-[6px] p-1`}>
 								<a href={`#${nav.id}`}>
 									{nav.title}
 								</a>
